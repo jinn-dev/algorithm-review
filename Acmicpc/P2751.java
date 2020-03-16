10f497abd088f36b46e3b8472ef40c15f1282add
jinn-dev <gsbyyy@naver.com>
2020-03-16 12:08:15 +0900

    셀프넘버 문제

--- Acmicpc/P2751.java
+++ Acmicpc/P2751.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 
 public class P2751 {
   //Q. N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
-  //Q. 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 숫자가 주어진다. 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+  //Input. 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 숫자가 주어진다. 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
   //P. Collections 내장 sort 메소드 사용
 	public static void main(String[] args) {
 		Main main = new Main();
