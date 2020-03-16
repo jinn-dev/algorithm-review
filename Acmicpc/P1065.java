62e3060218cc7a9e1fa465e98e63e3927c831e7c
jinn-dev <gsbyyy@naver.com>
2020-03-17 01:13:41 +0900

    Added P1065.java

---
+++ Acmicpc/P1065.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	/*
+	문제
+	어떤 양의 정수 X의 각 자리가 등차수열을 이룬다면, 그 수를 한수라고 한다. 등차수열은 연속된 두 개의 수의 차이가 일정한 수열을 말한다. N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오.
+
+	입력
+	첫째 줄에 1,000보다 작거나 같은 자연수 N이 주어진다.
+
+	출력
+	첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
+	*/
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String temp = br.readLine();
+		int N = Integer.parseInt(temp);	//0 < 1000 <= N
+		int CNT = 0;
+		int digitArr[] = new int[4];
+
+		for(int i=1; i<=N; i++) {
+
+			if(i < 100) {
+				CNT = i;
+			} else {
+				digitArr = digit(i);
+
+				//각자리 차가 같은 경우 등차수열로 판정하여 카운트
+				//1000은 제외해야 함
+				if(((digitArr[0]-digitArr[1]) == (digitArr[1]-digitArr[2])) && i != 1000) {
+					CNT++;
+				}
+			}
+		}
+
+		System.out.println(CNT);
+	}
+
+	//각 자리수 배열에 저장
+	private static int[] digit(int num) {
+		int idx = 0;
+		int temp = num;
+		int arr[] = new int[4];
+
+		while(temp > 0) {
+			arr[idx] = temp%10;
+			temp = temp/10;
+			idx++;
+		}
+
+		/*
+		for(int j=arr.length-1; j>=0; j--) {
+			System.out.print(arr[j]+" ");
+		}
+		*/
+		return arr;
+	}
+}
